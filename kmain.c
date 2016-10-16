846dc5f172e2f254c22f90d3fe3c51965ceaf8c0
Connor Stack <sirconnorstack@gmail.com>
2016-10-15 19:41:30 -0700

    Higher half kernel is able to call kmain().

--- kmain.c
+++ kmain.c
@@ -9,98 +9,100 @@
 #include "stdio.h"
 #include "types.h"
 
-static char *welcome_string = ""
-"                                                                                "
-"                                                                                "
-"                                                                                "
-"                                                                                "
-"                              | |                  | |                          "
-"                   ___   ___  | |_    __ _    ___  | | __                       "
-"                  / __| / __| | __|  / _` |  / __| | |/ /                       "
-"                 | (__  \\__ \\ | |_  | (_| | | (__  |   <                        "
-"                  \\___| |___/  \\__|  \\__,_|  \\___| |_|\\_\\                       "
-"                                                                                "
-"                                                                                "
-"                             ____     _____                                     "
-"                            / __ \\   / ____|                                    "
-"                           | |  | | | (___                                      "
-"                           | |  | |  \\___ \\                                     "
-"                           | |__| |  ____) |                                    "
-"                            \\____/  |_____/                                     "
-"                                                                                "
-"                                                                                "
-"                                                                                "
-"                                                                                "
-"                                                                                "
-"                                                                                "
-"                                                                                "
-"                                                                                "
-"";
+// static char *welcome_string = ""
+// "                                                                                "
+// "                                                                                "
+// "                                                                                "
+// "                                                                                "
+// "                              | |                  | |                          "
+// "                   ___   ___  | |_    __ _    ___  | | __                       "
+// "                  / __| / __| | __|  / _` |  / __| | |/ /                       "
+// "                 | (__  \\__ \\ | |_  | (_| | | (__  |   <                        "
+// "                  \\___| |___/  \\__|  \\__,_|  \\___| |_|\\_\\                       "
+// "                                                                                "
+// "                                                                                "
+// "                             ____     _____                                     "
+// "                            / __ \\   / ____|                                    "
+// "                           | |  | | | (___                                      "
+// "                           | |  | |  \\___ \\                                     "
+// "                           | |__| |  ____) |                                    "
+// "                            \\____/  |_____/                                     "
+// "                                                                                "
+// "                                                                                "
+// "                                                                                "
+// "                                                                                "
+// "                                                                                "
+// "                                                                                "
+// "                                                                                "
+// "                                                                                "
+// "";
 
 void kmain(uint32_t ebx) {
-  // GRUB passes info to the kernel through the ebx register
-  multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+  // // GRUB passes info to the kernel through the ebx register
+  // multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+  if(ebx){};
 
-  clear_screen();
+  // clear_screen();
 
-  printf(welcome_string);
+  // printf(welcome_string);
 
   serial_init();
   log("\n--------------------\ncstackOS is booting!\n--------------------\n\nInitialized serial port.\n");
+  while(1){};
 
-  initialize_gdt();
-  log("Loaded global descriptor table.\n");
+  // initialize_gdt();
+  // log("Loaded global descriptor table.\n");
 
-  initialize_idt();
-  log("Loaded interrupt descriptor table.\n");
+  // initialize_idt();
+  // log("Loaded interrupt descriptor table.\n");
 
-  log("Issuing test interrupt.\n");
-  interrupt(49);
-  log("Returned from test interrupt()\n");
+  // log("Issuing test interrupt.\n");
+  // interrupt(49);
+  // log("Returned from test interrupt()\n");
 
-  pic_init();
-  log("Initialized PIC\n");
+  // pic_init();
+  // log("Initialized PIC\n");
 
-  page_directory_t pd = initialize_page_directory();
-  log("Initialized page directory.\n");
-  log("Address of page directory: ");
-  print_uint32(LOG, (uint32_t) pd);
-  log("\n");
+  // page_directory_t pd = initialize_page_directory();
+  // log("Initialized page directory.\n");
+  // log("Address of page directory: ");
+  // print_uint32(LOG, (uint32_t) pd);
+  // log("\n");
 
-  page_table_t pt = get_page_table(pd, 0);
-  log("Address of first page table: ");
-  print_uint32(LOG, (uint32_t) pt);
-  log("\n");
+  // page_table_t pt = get_page_table(pd, 0);
+  // log("Address of first page table: ");
+  // print_uint32(LOG, (uint32_t) pt);
+  // log("\n");
 
-  log("First 5 page table entries: ");
-  for (int i = 0; i < 5; i++) {
-    print_uint32(LOG, pt[i]);
-    log("\n");
-  }
+  // log("First 5 page table entries: ");
+  // for (int i = 0; i < 5; i++) {
+  //   print_uint32(LOG, pt[i]);
+  //   log("\n");
+  // }
 
-  log("Setting page directory...\n");
-  set_page_directory(pd);
-  log("Set page directory.\n");
+  // log("Setting page directory...\n");
+  // set_page_directory(pd);
+  // log("Set page directory.\n");
 
-  log("Enabling paging...\n");
-  enable_paging();
-  log("Paging enabled.\n");
+  // log("Enabling paging...\n");
+  // enable_paging();
+  // log("Paging enabled.\n");
 
-  uint32_t * unmapped_address = (uint32_t *) 0x400000; // 4 MB
-  log("Trying to access address ");
-  print_uint32(LOG, (uint32_t) unmapped_address);
-  log(":\n");
-  print_uint8(LOG, *unmapped_address);
-  log("\n. Successfully accessed.");
+  // uint32_t * unmapped_address = (uint32_t *) 0x400000; // 4 MB
+  // log("Trying to access address ");
+  // print_uint32(LOG, (uint32_t) unmapped_address);
+  // log(":\n");
+  // print_uint8(LOG, *unmapped_address);
+  // log("\n. Successfully accessed.");
 
-  log("\nMultiboot info passed to kernel from GRUB:\n");
-  print_multiboot_info(LOG, mbinfo);
+  // log("\nMultiboot info passed to kernel from GRUB:\n");
+  // print_multiboot_info(LOG, mbinfo);
 
-  void_function_t start_program = first_module_as_a_function(mbinfo);
-  start_program();
-  log("Got past call to start_program()\n");
+  // void_function_t start_program = first_module_as_a_function(mbinfo);
+  // start_program();
+  // log("Got past call to start_program()\n");
 
-  // Loop forever
-  // User input is accepted asynchronously via interrupts
-  while(1){}
+  // // Loop forever
+  // // User input is accepted asynchronously via interrupts
+  // while(1){}
 }
--- loader.s
+++ loader.s
@@ -26,6 +26,29 @@ CHECKSUM     equ -(MAGIC_NUMBER + FLAGS)
 
 KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes (1 kilobyte)
 
+; This is the virtual base address of kernel space. It must be used to convert virtual
+; addresses into physical addresses until paging is enabled. Note that this is not
+; the virtual address where the kernel image itself is loaded -- just the amount that must
+; be subtracted from a virtual address to get a physical address.
+KERNEL_VIRTUAL_BASE equ 0xC0000000                  ; 3GB
+KERNEL_PAGE_NUMBER equ (KERNEL_VIRTUAL_BASE >> 22)  ; Page directory index of kernel's 4MB PTE.
+
+section .data
+align 0x1000
+BootPageDirectory:
+    ; This page directory entry identity-maps the first 4MB of the 32-bit physical address space.
+    ; All bits are clear except the following:
+    ; bit 7: PS The kernel page is 4MB.
+    ; bit 1: RW The kernel page is read/write.
+    ; bit 0: P  The kernel page is present.
+    ; This entry must be here -- otherwise the kernel will crash immediately after paging is
+    ; enabled because it can't fetch the next instruction! It's ok to unmap this page later.
+    dd 0x00000083
+    times (KERNEL_PAGE_NUMBER - 1) dd 0                 ; Pages before kernel space.
+    ; This page directory entry defines a 4MB page containing the kernel.
+    dd 0x00000083
+    times (1024 - KERNEL_PAGE_NUMBER - 1) dd 0  ; Pages after the kernel image.
+
 section .text                   ; start of the text (code) section
                                 ; .data is for initialized variables
                                 ; .bss is for uninitialized variables
@@ -41,9 +64,33 @@ align 4                         ; the code must be 4 byte aligned
     dd FLAGS                    ; the flags,
     dd CHECKSUM                 ; and the checksum
 
-loader:                         ; the loader label (defined as entry point in linker script)
-                                ; that means the address of this instruction will
-                                ; appear in the symbol table
+loader equ (_loader - KERNEL_VIRTUAL_BASE)
+_loader:
+    ; NOTE: Until paging is set up, the code must be position-independent and use physical
+    ; addresses, not virtual ones!
+    mov ecx, (BootPageDirectory - KERNEL_VIRTUAL_BASE) ; 0x104000
+    mov cr3, ecx                                        ; Load Page Directory Base Register.
+ 
+    mov ecx, cr4
+    or ecx, 0x00000010                          ; Set PSE bit in CR4 to enable 4MB pages.
+    mov cr4, ecx
+ 
+    mov ecx, cr0
+    or ecx, 0x80000000                          ; Set PG bit in CR0 to enable paging.
+    mov cr0, ecx
+ 
+    ; Start fetching instructions in kernel space.
+    ; Since eip at this point holds the physical address of this command (approximately 0x00100000)
+    ; we need to do a long jump to the correct virtual address of StartInHigherHalf which is
+    ; approximately 0xC0100000.
+    lea ecx, [StartInHigherHalf]
+    jmp ecx
+
+StartInHigherHalf:
+    ; Unmap the identity-mapped first 4MB of physical address space. It should not be needed
+    ; anymore.
+    mov dword [BootPageDirectory], 0
+    invlpg [0]
 
     mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                 ; stack (end of memory area)

a5b9565fb68c22b27113442f892c18a26288eeb2
Connor Stack <sirconnorstack@gmail.com>
2016-11-19 13:18:55 -0800

    Fibonacci user program.

--- user_program.c
+++ user_program.c
@@ -1,5 +1,17 @@
+int fib(unsigned int n) {
+  if (n == 0) {
+    return 0;
+  }
+
+  if (n == 1) {
+    return 1;
+  }
+
+  return fib(n-1) + fib(n-2);
+}
+
 int main() {
   if (1) {
-    return 1;
+    return fib(20); // Should return 0x1a6d
   }
 }
